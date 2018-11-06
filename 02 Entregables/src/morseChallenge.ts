3ca2584100ba8714abbfc6012170e722c1354516
jacxon4 <jacxon4@gmail.com>
2018-11-06 21:44:39 +0100

    - Transmit inicial - No timeout funcional, - Pendiente utilizar la unit

--- 02 Entregables/src/morseChallenge.ts
+++ 02 Entregables/src/morseChallenge.ts
@@ -96,35 +96,34 @@ interface MorseSymbol<T> {
 }
 
 interface MorseTransmitter {
-    morseAlphabet: MorseSymbol<string>;
-    writer: (message: string) => void;
-    timeOut: (unit: number) => void;
-    timeUnit?: number;
-    translate: (message: string)=> string[][];
-    //transmit:(message: string) => void;
+    transmit: (message: string)=> void;
 
 }
 
-const timeOut = (unit: number) => (message: string) => setTimeout(writer(message), unit * 50);
+const timeOut = (unit: number) => (message: string) => setTimeout(writer(message), unit * 100);
 const dashTimeOut = timeOut(3);
 const letterTimeOut = timeOut(3);
+const dotTimeOut = timeOut(1);
 const wordTimeOut = timeOut(7);
-const writer = (message) => console.log(message);
+const writer = (message: string) => console.log(message);
 const splitWords = (message: string): string[] => message.split(' ');
 const splitLetters = (message: string): string[] => message.split('');
-const map = (dictionary) => (message: string) => splitWords(message).map((word) => splitLetters(word).map((letter) => dictionary[letter]));
-
-const createMorseTransmitter = (morseAlphabet: MorseSymbol<string>, timeUnit: number = 10) => ({
-    morseAlphabet,
-    timeUnit,
-    writer,
-    timeOut,
-    translate: (message)=> map(morseAlphabet)(message),
-    //transmit: (callback) => callback(),
+const translate = (dictionary) => (message: string) => splitWords(message).map((word) => splitLetters(word).map((letter) => dictionary[letter]));
 
 
+const createMorseTransmitter = (morseAlphabet: MorseSymbol<string>, timeUnit: number = 100): MorseTransmitter => ({
+    transmit: (message)=> translate(morseAlphabet)(message).forEach((wordArr: string[]) => {
+         wordArr.forEach((letter: string) => {
+            letter.split('').forEach( (symbol: string) => {
+                (symbol === "-") ? dashTimeOut("ON") : dotTimeOut("ON");
+                dotTimeOut("OFF");
+            });
+            letterTimeOut("OFF");
+        });
+        wordTimeOut("OFF");
+    }),
 });
 
 
 let transmitter: MorseTransmitter = createMorseTransmitter(morseAlphabet, 100);
-console.log(transmitter);
+console.log('transmitter', transmitter);
